7cb72ad08dc690f34b8f9caf732f1867429d8786
Christian Souza <christianyugioh@gmail.com>
2021-04-26 19:17:50 -0300

    Projeto iniciado mostrando a mensagem Olá Mundo

---
+++ new project test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace new_project_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
